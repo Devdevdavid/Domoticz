2851a0a3555f0886a6f5c6a5f3a59691dc0a9895
David Devant <david.devant@live.fr>
2019-10-05 14:39:37 +0200

    v1.6.1 Add the 2nd relay impulsion when alert goes ON

--- interface_temperature/software/src/script/script.cpp
+++ interface_temperature/software/src/script/script.cpp
@@ -6,7 +6,13 @@
 extern uint32_t tick;
 uint32_t nextTempCheckTick = 0;
 uint32_t nextDomoticzUpdateTick = 0;
+uint32_t nextSecondRelayImpulsTick = UINT32_MAX; // Disabled at startup
 bool isInAlertOld = false;
+
+// Sub script function prototypes
+#ifdef MODULE_RELAY
+void script_send_relay_impulse(uint32_t impulseDurationMs);
+#endif
 
 void script_execute(void)
 {
@@ -19,6 +25,7 @@ void script_execute(void)
         domoticz_send_temperature(DOMOTICZ_SENSOR_ID_OUTSIDE, temp_get_value(DEVICE_INDEX_1));
     }
 #endif
+    // This is only to test buttons 
     if (_isset(STATUS_BUTTON, STATUS_BUTTON_RISING)) {
         _unset(STATUS_BUTTON, STATUS_BUTTON_RISING);
         log_info("Rising edge");
@@ -26,10 +33,6 @@ void script_execute(void)
     if (_isset(STATUS_BUTTON, STATUS_BUTTON_FALLING)) {
         _unset(STATUS_BUTTON, STATUS_BUTTON_FALLING);
         log_info("Falling edge");
-    }
-    if (_isset(STATUS_BUTTON, STATUS_BUTTON_LONG_HOLD)) {
-        _unset(STATUS_BUTTON, STATUS_BUTTON_LONG_HOLD);
-        log_info("Long hold edge");
     }
 
 #ifdef MODULE_TEMPERATURE
@@ -55,9 +58,20 @@ void script_execute(void)
             isInAlertOld = _isset(STATUS_SCRIPT, STATUS_SCRIPT_IN_ALERT);
             log_error("Script alert is now %s", isInAlertOld ? "active" : "inactive");
 #ifdef MODULE_RELAY
-            relay_set_state(isInAlertOld);
+            // Are we in impulsion mode ? (OPT jumper)
             if (_isset(STATUS_BUTTON, STATUS_BUTTON_IS_PRESSED)) {
-                relay_set_toogle_timeout(SCRIPT_RELAY_IMPULSION_DURATION);
+                script_send_relay_impulse(SCRIPT_RELAY_IMPULSION_DURATION);
+
+                // If 2nd impulsion is activated and the alert is going from OFF to ON then
+                // prepare to send a 2nd impulsion
+#if (SCRIPT_RELAY_MS_BEFORE_2ND_IMPULSION != 0)
+                if (isInAlertOld) {
+                    nextSecondRelayImpulsTick = tick + SCRIPT_RELAY_MS_BEFORE_2ND_IMPULSION;
+                }
+#endif
+            } else {
+                // No impulsion, just set the relay ON when alert is ON and vice versa
+                relay_set_state(isInAlertOld);
             }
 #endif
 #ifdef MODULE_BUZZER
@@ -65,5 +79,28 @@ void script_execute(void)
 #endif
         }
     }
+
+    // This is the 2nd relay impulsion callback (Activated only when 2nd impulsion duration is > 0)
+#if defined(MODULE_RELAY) && (SCRIPT_RELAY_MS_BEFORE_2ND_IMPULSION != 0)
+    if (tick > nextSecondRelayImpulsTick) {
+        nextSecondRelayImpulsTick = UINT32_MAX; // Disable second relay impulse
+        script_send_relay_impulse(SCRIPT_RELAY_IMPULSION_DURATION);
+    }
+#endif
 #endif
 }
+
+/**
+ * @brief Set the relay ON then turn it OFF after impulseDurationMs milliseconds
+ * 
+ * @param impulseDurationMs 
+ */
+#ifdef MODULE_RELAY
+void script_send_relay_impulse(uint32_t impulseDurationMs)
+{
+    // Set relay to ON
+    relay_set_state(true);
+    // Turn it OFF after impulseDurationMs milliseconds
+    relay_set_toogle_timeout(impulseDurationMs);
+}
+#endif
