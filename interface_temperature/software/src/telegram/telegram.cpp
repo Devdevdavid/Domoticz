b2af7b1b3b2c427a4d69ead241eda0dfde9bcfad
David DEVANT <david.devant@outlook.com>
2020-08-05 16:04:27 +0200

    Sending telegram message is ok

--- interface_temperature/software/src/telegram/telegram.cpp
+++ interface_temperature/software/src/telegram/telegram.cpp
@@ -10,11 +10,10 @@
 extern uint32_t tick;
 
 // STATIC
-static uint32_t nextCheckTick = UINT32_MAX;
-static String ThisChat;
-
-WiFiClientSecure client;
-UniversalTelegramBot TBot(TELEGRAM_CONV_TOKEN, client);
+static uint32_t nextCheckTick = 0;
+static String linkedChat;
+static WiFiClientSecure wiFiClientSecure;
+static UniversalTelegramBot TBot(TELEGRAM_CONV_TOKEN, wiFiClientSecure);
 
 static void telegram_msg_send_motd(void)
 {
@@ -33,27 +32,29 @@ static void telegram_msg_send_motd(void)
 	welcome += "/Skip : No action\n";
 	welcome += "/status : Return connection state\n";
 #endif
-	TBot.sendMessage(ThisChat, welcome, "Markdown");// mise en forme du texte
+	TBot.sendMessage(linkedChat, welcome, "Markdown");// mise en forme du texte
 }
 
 static void telegram_handle_new_message(telegramMessage * message) {
 	// Check command received
 	if ((message->text == "/Arret") || (message->text == "/Off")) {
 		String keyboardJson = "[[\"/status\"]]";
-		TBot.sendMessageWithReplyKeyboard(ThisChat, TG_MSG_CHOOSE_OPTION, "", keyboardJson, true);
+		TBot.sendMessageWithReplyKeyboard(linkedChat, TG_MSG_CHOOSE_OPTION, "", keyboardJson, true);
+
+		// Unlink client
+		linkedChat = "";
 	}
 	else if ((message->text == "/statut") || (message->text == "/status")) {
-		TBot.sendMessage(ThisChat, TG_MSG_CONNECTION_OK, "");
+		TBot.sendMessage(linkedChat, TG_MSG_CONNECTION_OK, "");
 	}
 	else if (message->text == "/start") {
 		telegram_msg_send_motd();
 
-		if (ThisChat=="") {
-			ThisChat = String(message->chat_id);
-		}
+		// Link the new client
+		linkedChat = String(message->chat_id);
 
 	    String keyboardJson = "[[\"/status\"]]";
-	    TBot.sendMessageWithReplyKeyboard(ThisChat, TG_MSG_CHOOSE_OPTION, "", keyboardJson, true);
+	    TBot.sendMessageWithReplyKeyboard(linkedChat, TG_MSG_CHOOSE_OPTION, "", keyboardJson, true);
 	}
 }
 
@@ -62,6 +63,13 @@ static void telegram_handle_new_message(telegramMessage * message) {
 void telegram_init(void)
 {
 	nextCheckTick = 0;
+
+	// This is the simplest way of getting this working
+  	// if you are passing sensitive information, or controlling
+  	// something important, please either use certStore or at
+  	// least client.setFingerPrint
+	// https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot/issues/104#issuecomment-485255312
+	wiFiClientSecure.setInsecure();
 }
 
 void telegram_main(void)
@@ -70,20 +78,28 @@ void telegram_main(void)
 
 	if (tick >= nextCheckTick)  {
 		nextCheckTick = tick + TELEGRAM_CHECK_PERIOD_MS;
+		log_info("TBot Update %d", tick);
 
 		do {
 			msgNumber = TBot.getUpdates(TBot.last_message_received + 1);
 			for (int i = 0; i < msgNumber; i++) {
+				log_info("Handling TBot message %d: %s", i, TBot.messages[i].text.c_str());
 				telegram_handle_new_message(&TBot.messages[i]);
 			}
-		} while(msgNumber);
+		} while (msgNumber);
 	}
 }
 
 void telegram_send_msg_temperature(uint8_t sensorID, float degreesValue)
 {
-	String msg = String(sensorID) + "]" + TG_MSG_TEMPERATURE_IS + String(degreesValue) + "'C";
-	TBot.sendMessage(ThisChat, msg, "");
+	// We didn't start yet
+	if (linkedChat == "") {
+		return;
+	}
+
+	String msg = String(sensorID) + "] " + TG_MSG_TEMPERATURE_IS + String(degreesValue) + "'C";
+	TBot.sendMessage(linkedChat, msg, "");
+	log_info("Sending \"%s\" to %s", msg.c_str(), linkedChat.c_str());
 }
 
 #endif
