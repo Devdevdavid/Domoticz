82168ba4f97cb35039cac91a40976887164286e4
David DEVANT <david.devant@live.fr>
2021-04-17 19:22:00 +0200

    Add CLI commands

--- lightkit/software/src/cmd/term.cpp
+++ lightkit/software/src/cmd/term.cpp
@@ -39,197 +39,223 @@ int print_args(uint8_t argc, char * argv[])
 	return 0;
 }
 
+static int call_reset(uint8_t argc, char * argv[])
+{
+	cmd_reset_module();
+	return 0;
+}
+
+static int call_print_status(uint8_t argc, char * argv[])
+{
+	cmd_print_status();
+	return 0;
+}
+
+static int call_flash_setting_reset(uint8_t argc, char * argv[])
+{
+	return cmd_flash_setting_reset();
+}
+
+#ifdef MODULE_STRIPLED
+static int call_stripled_set_brightness(uint8_t argc, char * argv[])
+{
+	if (strcmp(argv[0], "auto") == 0) {
+		cmd_set_brightness_auto(true);
+	} else {
+		uint8_t brightness = strtol(argv[0], NULL, 10);
+		if (brightness > 100) {
+			term_print("Bad brightness level: " + String(brightness));
+			return -1;
+		}
+		cmd_set_brightness(brightness);
+	}
+	return 0;
+}
+
+static int call_stripled_set_state(uint8_t argc, char * argv[])
+{
+	if (strcmp(argv[0], "on") == 0) {
+		return cmd_set_state(true);
+	} else if (strcmp(argv[0], "off") == 0) {
+		return cmd_set_state(false);
+	} else if (strcmp(argv[0], "toggle") == 0) {
+		return cmd_set_state(!cmd_get_state());
+	} else {
+		term_print("Unknown argument: " + String(argv[0]));
+		return -1;
+	}
+}
+
+static int call_stripled_set_demo_mode(uint8_t argc, char * argv[])
+{
+	if (strcmp(argv[0], "on") == 0) {
+		return cmd_set_demo_mode(true);
+	} else if (strcmp(argv[0], "off") == 0) {
+		return cmd_set_demo_mode(false);
+	} else if (strcmp(argv[0], "toggle") == 0) {
+		return cmd_set_demo_mode(!cmd_get_demo_mode());
+	} else {
+		term_print("Unknown argument: " + String(argv[0]));
+		return -1;
+	}
+}
+
+static int call_stripled_set_anim(uint8_t argc, char * argv[])
+{
+	if (strcmp(argv[0], "off") == 0) {
+		return cmd_set_animation(0);
+	} else {
+		uint8_t animID = strtol(argv[0], NULL, 10);
+		return cmd_set_animation(animID);
+	}
+}
+#endif
+
+#ifdef MODULE_RELAY
+static int call_relay_set_state(uint8_t argc, char * argv[])
+{
+	if (strcmp(argv[0], "on") == 0) {
+		return relay_set_state(true);
+	} else if (strcmp(argv[0], "off") == 0) {
+		return relay_set_state(false);
+	} else if (strcmp(argv[0], "toggle") == 0) {
+		return relay_set_state(!relay_get_state());
+	} else {
+		term_print("Unknown argument: " + String(argv[0]));
+		return -1;
+	}
+}
+#endif
+
+#ifdef MODULE_STATUS_LED
+static int call_status_led_set_state(uint8_t argc, char * argv[])
+{
+	if (strcmp(argv[0], "on") == 0) {
+		return cmd_set_status_led(true);
+	} else if (strcmp(argv[0], "off") == 0) {
+		return cmd_set_status_led(false);
+	} else if (strcmp(argv[0], "toggle") == 0) {
+		return cmd_set_status_led(!_isset(STATUS_APPLI, STATUS_APPLI_STATUS_LED));
+	} else {
+		term_print("Unknown argument: " + String(argv[0]));
+		return -1;
+	}
+}
+#endif
+
 /**
  * @brief Create the command line interface
  */
 static int term_create_cli_commands(void)
 {
 	cli_token * tokRoot = cli_get_root_token();
-	cli_token * tokLan;
-	cli_token * tokIpSet;
+	cli_token * tokLvl1;
+	cli_token * tokLvl2;
 	cli_token * curTok;
 
 	// Create commands
-	curTok = cli_add_token("exit", "Exit application");
-	cli_set_callback(curTok, &print_args);
-	cli_add_children(tokRoot, curTok);
+	tokLvl1 = cli_add_token("board", "Board configuration");
+	{
+		curTok = cli_add_token("reset", "Reset module");
+		cli_set_callback(curTok, &call_reset);
+		cli_add_children(tokLvl1, curTok);
 
-	tokLan = cli_add_token("lan", "LAN configuration");
+		curTok = cli_add_token("status", "Print current status");
+		cli_set_callback(curTok, &call_print_status);
+		cli_add_children(tokLvl1, curTok);
+	}
+	cli_add_children(tokRoot, tokLvl1);
+
+	tokLvl1 = cli_add_token("flash", "Manage flash memory");
+	{
+		curTok = cli_add_token("default", "Reset flash setting to default");
+		cli_set_callback(curTok, &call_flash_setting_reset);
+		cli_add_children(tokLvl1, curTok);
+	}
+	cli_add_children(tokRoot, tokLvl1);
+
+#ifdef MODULE_STRIPLED
+	tokLvl1 = cli_add_token("strip", "Manage stripled");
+	{
+		curTok = cli_add_token("brightness", "[level|auto] Set the brightness");
+		cli_set_callback(curTok, &call_stripled_set_brightness);
+		cli_set_argc(curTok, 1, 0);
+		cli_add_children(tokLvl1, curTok);
+
+		curTok = cli_add_token("state", "[on|off|toggle] Set state of the stripled");
+		cli_set_callback(curTok, &call_stripled_set_state);
+		cli_set_argc(curTok, 1, 0);
+		cli_add_children(tokLvl1, curTok);
+
+		curTok = cli_add_token("demo", "[on|off|toggle] Set demo mode");
+		cli_set_callback(curTok, &call_stripled_set_demo_mode);
+		cli_set_argc(curTok, 1, 0);
+		cli_add_children(tokLvl1, curTok);
+
+		curTok = cli_add_token("anim", "[animID|off] Set the animation ID");
+		cli_set_callback(curTok, &call_stripled_set_anim);
+		cli_set_argc(curTok, 1, 0);
+		cli_add_children(tokLvl1, curTok);
+	}
+	cli_add_children(tokRoot, tokLvl1);
+#endif
+
+#ifdef MODULE_RELAY
+	tokLvl1 = cli_add_token("relay", "Manage relay");
+	{
+		curTok = cli_add_token("set", "[on|off|toggle] Set the relay state");
+		cli_set_callback(curTok, &call_relay_set_state);
+		cli_set_argc(curTok, 1, 0);
+		cli_add_children(tokLvl1, curTok);
+	}
+	cli_add_children(tokRoot, tokLvl1);
+#endif
+
+#ifdef MODULE_STATUS_LED
+	tokLvl1 = cli_add_token("led", "Manage LED");
+	{
+		curTok = cli_add_token("set", "[on|off|toggle] Set the relay state");
+		cli_set_callback(curTok, &call_status_led_set_state);
+		cli_set_argc(curTok, 1, 0);
+		cli_add_children(tokLvl1, curTok);
+	}
+	cli_add_children(tokRoot, tokLvl1);
+#endif
+
+	tokLvl1 = cli_add_token("lan", "LAN configuration");
 	{
 		curTok = cli_add_token("show", "[interface] Show configuration");
 		cli_set_callback(curTok, &print_args);
 		cli_set_argc(curTok, 0, 1);
-		cli_add_children(tokLan, curTok);
+		cli_add_children(tokLvl1, curTok);
 
-		tokIpSet = cli_add_token("set", "Define new configuration");
+		tokLvl2 = cli_add_token("set", "Define new configuration");
 		{
 			curTok = cli_add_token("ip", "<address> Set IP adress");
 			cli_set_callback(curTok, &print_args);
 			cli_set_argc(curTok, 1, 0);
-			cli_add_children(tokIpSet, curTok);
+			cli_add_children(tokLvl2, curTok);
 
 			curTok = cli_add_token("gateway", "<address> Set gateway adress");
 			cli_set_callback(curTok, &print_args);
 			cli_set_argc(curTok, 1, 0);
-			cli_add_children(tokIpSet, curTok);
+			cli_add_children(tokLvl2, curTok);
 
 			curTok = cli_add_token("mask", "<address> Set network mask");
 			cli_set_callback(curTok, &print_args);
 			cli_set_argc(curTok, 1, 0);
-			cli_add_children(tokIpSet, curTok);
+			cli_add_children(tokLvl2, curTok);
 
-			curTok =
-			cli_add_token("proxy", "<address> <port> Set the proxy location");
+			curTok = cli_add_token("proxy", "<address> <port> Set the proxy location");
 			cli_set_callback(curTok, &print_args);
 			cli_set_argc(curTok, 2, 0);
-			cli_add_children(tokIpSet, curTok);
+			cli_add_children(tokLvl2, curTok);
 		}
-		cli_add_children(tokLan, tokIpSet);
+		cli_add_children(tokLvl1, tokLvl2);
 	}
-	cli_add_children(tokRoot, tokLan);
+	cli_add_children(tokRoot, tokLvl1);
 	return 0;
 }
-
-#ifdef DISABLEDNONO
-static void term_execute_command(void)
-{
-	// If command is empty, display help menu
-	if (termPort.rxLength == 0) {
-		termPort.rxBuffer[0] = 'H';
-		termPort.rxLength    = 0;
-	}
-
-	// Get a new line for awnser
-	switch (termPort.rxBuffer[0]) {
-	case 'H':
-		cmd_print_help();
-		break;
-	case 'I':
-		cmd_reset_module();
-		break;
-	case 'T':
-		cmd_print_status();
-		break;
-	case 'F':
-		if (cmd_flash_setting_reset()) {
-			_term_ack(ERROR_INTERNAL);
-		} else {
-			_term_ack(OK);
-		}
-		break;
-#ifdef MODULE_STATUS_LED
-	case 'L':
-		if (termPort.rxLength == 1) {
-			cmd_set_status_led(!_isset(STATUS_APPLI, STATUS_APPLI_STATUS_LED));
-		} else if (termPort.rxLength == 2) {
-			cmd_set_status_led(sToU16(&termPort.rxBuffer[1], 1));
-		} else {
-			_term_ack(ERROR_WRONG_LENGTH);
-			break;
-		}
-		_term_ack(OK);
-		break;
-#endif
-#ifdef MODULE_STRIPLED
-	case 'B':
-		if (termPort.rxLength < 1) {
-			_term_ack(ERROR_WRONG_LENGTH);
-		} else if (termPort.rxLength == 1) {
-			cmd_set_brightness_auto(true);
-			_term_ack(OK);
-		} else {
-			uint8_t brightness = sToU16(&termPort.rxBuffer[1], termPort.rxLength - 1);
-			if (brightness <= 100) {
-				cmd_set_brightness(brightness);
-				_term_ack(OK);
-			} else {
-				_term_ack(ERROR_WRONG_VALUE);
-			}
-		}
-		break;
-#endif
-#ifdef MODULE_STRIPLED
-	/** SWITCH: ON/OFF */
-	case 'S':
-		if (termPort.rxLength < 1) {
-			_term_ack(ERROR_WRONG_LENGTH);
-		} else if (termPort.rxLength == 1) {
-			/** TOGGLE STATE */
-			cmd_set_state(!cmd_get_state());
-			_term_ack(OK);
-		} else {
-			if (termPort.rxBuffer[1] == '1') {
-				/** SET to ON */
-				cmd_set_state(true);
-				_term_ack(OK);
-			} else if (termPort.rxBuffer[1] == '0') {
-				/** SET to OFF */
-				cmd_set_state(false);
-				_term_ack(OK);
-			} else {
-				_term_ack(ERROR_WRONG_VALUE);
-			}
-		}
-		break;
-#endif
-#ifdef MODULE_STRIPLED
-	/** DEMO MODE: ON/OFF */
-	case 'D':
-		if (termPort.rxLength < 1) {
-			_term_ack(ERROR_WRONG_LENGTH);
-		} else if (termPort.rxLength == 1) {
-			/** TOGGLE STATE */
-			cmd_set_demo_mode(!cmd_get_demo_mode());
-			_term_ack(OK);
-		} else {
-			if (termPort.rxBuffer[1] == '1') {
-				/** SET to ON */
-				cmd_set_demo_mode(true);
-				_term_ack(OK);
-			} else if (termPort.rxBuffer[1] == '0') {
-				/** SET to OFF */
-				cmd_set_demo_mode(false);
-				_term_ack(OK);
-			} else {
-				_term_ack(ERROR_WRONG_VALUE);
-			}
-		}
-		break;
-#endif
-#ifdef MODULE_STRIPLED
-	/** ANIMATION */
-	case 'A':
-		if (termPort.rxLength <= 1) {
-			_term_ack(ERROR_WRONG_LENGTH);
-		} else {
-			uint16_t animID = sToU16(&termPort.rxBuffer[1], termPort.rxLength - 1);
-			if (cmd_set_animation(animID) == OK) {
-				_term_ack(OK);
-			} else {
-				_term_ack(ERROR_WRONG_VALUE);
-			}
-		}
-		break;
-#endif
-#ifdef MODULE_RELAY
-	case 'R':
-		if (termPort.rxLength == 1) {
-			relay_set_state(!relay_get_state());
-		} else if (termPort.rxLength == 2) {
-			relay_set_state(sToU16(&termPort.rxBuffer[1], 1));
-		} else {
-			_term_ack(ERROR_WRONG_LENGTH);
-			break;
-		}
-		_term_ack(OK);
-		break;
-#endif
-	default:
-		_term_ack(ERROR_UNKNOWN_CMD);
-		break;
-	}
-}
-#endif
 
 //========================
 //	  PUBLIC FUNCTIONS
